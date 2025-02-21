60e5fff56c0d672acc64e20d33291c6f5c3cd825
Adrian <adriansanduta@gmail.com>
2025-02-20 20:36:06 -0600

    palindrome 1.1

--- questions/palindrome/index.js
+++ questions/palindrome/index.js
@@ -9,9 +9,18 @@
  * palindrome('frontend') === false
  */
 
-const palindrome = (str) => {
+/* 
+ const palindrome = (str) => {
   const reversed = str.split('').reverse().join('');
   return str === reversed;
+};
+
+module.exports = palindrome; */
+
+const palindrome = (str) => {
+ return str.split('').every((char, index) => {
+    return char === str[str.length -1 - index]
+  })
 };
 
 module.exports = palindrome;
