17fe5353c81cd2988c9dce8b6e9233db62873a60
Adrian <adriansanduta@gmail.com>
2025-02-20 20:04:46 -0600

    1.5

--- playground.js
+++ playground.js
@@ -1,7 +1,5 @@
 const arr = [1, -1, 2, 3];
 
-const sum = arr.reduce((accumulator, currentValue) => (
-    accumulator + currentValue
-))
+const sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue);
 
 console.log(sum); 
