06f29d7873c4d85cb7e90a6d945e7fa773cc9c5b
Adrian <adriansanduta@gmail.com>
2025-02-23 18:29:25 -0600

    maxChar1.0

--- questions/maxChar/index.js
+++ questions/maxChar/index.js
@@ -7,7 +7,20 @@
  */
 
 const maxCharacter = (str) => {
-  
+  const charMap = {};
+  let max = 0;
+    let maxChar = "";
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+  for(let char in charMap){
+    if(charMap[char] > max){
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
 };
 
 module.exports = maxCharacter;
