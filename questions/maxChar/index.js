d6788762352be36573c3f358aa982d464e79cb08
Adrian <adriansanduta@gmail.com>
2025-02-23 18:40:41 -0600

    maxChar1.1

--- questions/maxChar/index.js
+++ questions/maxChar/index.js
@@ -6,7 +6,7 @@
  * maxCharacter('foo 111123') === '1'
  */
 
-const maxCharacter = (str) => {
+/* const maxCharacter = (str) => {
   const charMap = {};
   let max = 0;
     let maxChar = "";
@@ -23,4 +23,23 @@ const maxCharacter = (str) => {
   return maxChar;
 };
 
-module.exports = maxCharacter;
+module.exports = maxCharacter; */
+
+const maxCharacter = (str) => {
+    const charMap = new Map();
+    let max = 0;
+      let maxChar = "";
+  
+    for (let char of str) {
+      charMap.set(char, charMap.get(char) + 1 || 1);
+    }
+    for(const [char, count] of charMap){
+      if(count > max){
+        max = count;
+        maxChar = char;
+      }
+    }
+    return maxChar;
+  };
+  
+  module.exports = maxCharacter;
