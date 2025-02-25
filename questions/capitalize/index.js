4850cc89d14f6cbe680be03f1b736eeacf446890
Adrian <adriansanduta@gmail.com>
2025-02-24 19:33:51 -0600

    capitalized1.3

--- questions/capitalize/index.js
+++ questions/capitalize/index.js
@@ -7,13 +7,26 @@
  * capitalize('i love frontend simplified') === 'I Love Frontend Simplified'
  */
 
-const capitalize = (str) => {
+/** const capitalize = (str) => {
     const result = [];
     for (let word of str.split(" ")) {
         const capitalizedWord = word[0].toUpperCase() + word.slice(1);
         result.push(capitalizedWord)
     }
   return result.join(" ");
+};
+
+module.exports = capitalize; */
+
+const capitalize = (str) => {
+  let result = str[0].toUpperCase();
+
+  for(let i = 1; i < str.length; ++i){
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } 
+    else {
+  }
 };
 
 module.exports = capitalize;
