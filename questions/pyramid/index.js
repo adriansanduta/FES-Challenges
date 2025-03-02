c5d882e3fff4b1a6693a562fdf18565585dcd017
Adrian <adriansanduta@gmail.com>
2025-03-01 20:20:55 -0600

    pyramid1.0

--- questions/pyramid/index.js
+++ questions/pyramid/index.js
@@ -18,7 +18,16 @@
  */
 
 const pyramid = (n) => {
-  
+  const columnWidth = 2 * n -1;
+
+  for (let row = 0; row < n; row++) {
+    let step = "";
+
+    for ( let column = 0; column < columnWidth; column++) {
+        step += "#";
+    } 
+    console.log(step)
+  }
 };
 
 module.exports = pyramid;
