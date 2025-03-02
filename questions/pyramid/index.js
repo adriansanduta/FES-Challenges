156a4fa0201001f0589bbe11d2612fa97522a1a9
Adrian <adriansanduta@gmail.com>
2025-03-01 20:23:24 -0600

    pyramid1.1

--- questions/pyramid/index.js
+++ questions/pyramid/index.js
@@ -19,11 +19,14 @@
 
 const pyramid = (n) => {
   const columnWidth = 2 * n -1;
-
+  const middle = Math.floor(columnWidth / 2);
   for (let row = 0; row < n; row++) {
     let step = "";
 
     for ( let column = 0; column < columnWidth; column++) {
+        if (column >= middle - row && column <= middle + row) {
+            step += "#";
+        }
         step += "#";
     } 
     console.log(step)
